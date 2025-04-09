37405bfabad03609cbfe15f0b0f2afafee7fd8ca
Tsche <che@pydong.org>
2025-04-09 06:44:22 +0200

    variant: fix adl bug

--- test/variant/variant.visit/incomplete.cpp
+++ test/variant/variant.visit/incomplete.cpp
@@ -9,10 +9,10 @@ struct Wrapper { T obj; };
 TEST(Visit, Incomplete){
   //! TODO
 
-  // auto obj = rsl::variant<Wrapper<Incomplete>*, int>{};
+  auto obj = rsl::variant<Wrapper<Incomplete>*, int>{};
 
-  // rsl::visit([](auto){}, obj);
-  // rsl::visit([](auto) -> Wrapper<Incomplete>* { return nullptr; }, obj);
-  // rsl::visit<void>([](auto){}, obj);
-  // rsl::visit<void*>([](auto) -> Wrapper<Incomplete>* { return nullptr; }, obj);
+  rsl::visit<void>([](auto){}, obj);
+  rsl::visit([](auto) -> Wrapper<Incomplete>* { return nullptr; }, obj);
+  rsl::visit<void>([](auto){}, obj);
+  rsl::visit<void*>([](auto) -> Wrapper<Incomplete>* { return nullptr; }, obj);
 }
