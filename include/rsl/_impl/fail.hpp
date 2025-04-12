c57f8662b9d436908c2f88dc50110b914fa9e1b3
Tsche <che@pydong.org>
2025-04-12 04:16:02 +0200

    add conan test pkg, remove is_within_lifetime dependency

---
+++ include/rsl/_impl/fail.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <experimental/meta>
+#include <string_view>
+
+
+namespace rsl::_impl {
+
+namespace _fail {
+template <char const* message>
+consteval void fail() {
+    static_assert(false, std::string_view{message});
+}
+}
+
+consteval void fail(std::string_view message) {
+    using std::meta::reflect_value;
+    using std::meta::define_static_string;
+    extract<void(*)()>(substitute(^^_fail::fail, {reflect_value(define_static_string(message))}))();
+}
+}
