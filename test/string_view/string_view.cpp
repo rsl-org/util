9a9d5eb440a22c6a9e2f8973a9a19f030b4090ae
Yaraslau Tamashevich <yaraslau.tamashevich@gmail.com>
2025-08-28 17:59:05 +0200

    Add tests for rsl::string_view

---
+++ test/string_view/string_view.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include <rsl/string_view>
+
+TEST(StringView, Empty) {
+  ASSERT_TRUE(rsl::string_view().empty());
+  ASSERT_TRUE(!rsl::string_view("a").empty());
+}
+
+TEST(StringView, Size) {
+  ASSERT_EQ(rsl::string_view().size(), 0);
+  ASSERT_EQ(rsl::string_view("a").size(), 1);
+  ASSERT_EQ(rsl::string_view("abc").size(), 3);
+}
+
+TEST(StringView, Compare) {
+  ASSERT_TRUE(rsl::string_view() == rsl::string_view());
+  ASSERT_TRUE(rsl::string_view("a") == rsl::string_view("a"));
+  ASSERT_TRUE(rsl::string_view("a") != rsl::string_view());
+  ASSERT_TRUE(rsl::string_view("a") != rsl::string_view("b"));
+}
+
+TEST(StringView, Misc) {
+  ASSERT_TRUE(sizeof(rsl::string_view::value_type) == 1);
+  ASSERT_TRUE(rsl::string_view("a").max_size() == std::numeric_limits<size_t>::max());
+}
