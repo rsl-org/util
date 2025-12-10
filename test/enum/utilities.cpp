88c8748b51c7279b909bb4d6e515453a5bc93f86
Tsche <che@pydong.org>
2025-12-10 01:08:13 +0000

    implement is_valid_enumerator

--- test/enum/utilities.cpp
+++ test/enum/utilities.cpp
@@ -2,14 +2,54 @@
 #include <gtest/gtest.h>
 
 namespace {
-enum class Foo : uint8_t {
+enum Foo {
+  foo,
+  bar = 10
+};
+
+enum Bar {
+  foo1 = -1,
+  foo2 = 5
+};
+
+enum class [[=rsl::flag_enum]] Baz : int {
+  foo,
+  bar = 10
+};
+
+enum class Zoinks : int {
   foo,
   bar = 10
 };
 }
 
 TEST(Enum, IsNamed) {
-  ASSERT_TRUE(rsl::is_named_enumerator(Foo::bar));
-  ASSERT_TRUE(rsl::is_named_enumerator(Foo::foo));
-  ASSERT_FALSE(rsl::is_named_enumerator(Foo(20)));
+  ASSERT_TRUE(rsl::is_named_enumerator<Foo>(Foo::bar));
+  ASSERT_TRUE(rsl::is_named_enumerator<Foo>(Foo::foo));
+  ASSERT_FALSE(rsl::is_named_enumerator<Foo>(20));
+}
+
+TEST(Enum, IsValid) {
+  ASSERT_TRUE(rsl::is_valid_enumerator<Foo>(Foo::foo));
+  ASSERT_TRUE(rsl::is_valid_enumerator<Foo>(Foo::bar));
+  ASSERT_TRUE(rsl::is_valid_enumerator<Foo>(Foo::foo | Foo::bar));
+  ASSERT_TRUE(rsl::is_valid_enumerator<Foo>(15));
+  ASSERT_FALSE(rsl::is_valid_enumerator<Foo>(16));
+
+  ASSERT_TRUE(rsl::is_valid_enumerator<Bar>(Bar::foo1));
+  ASSERT_TRUE(rsl::is_valid_enumerator<Bar>(Bar::foo2));
+  ASSERT_TRUE(rsl::is_valid_enumerator<Bar>(Bar(-8)));
+  ASSERT_TRUE(rsl::is_valid_enumerator<Bar>(Bar(7)));
+  ASSERT_FALSE(rsl::is_valid_enumerator<Bar>(-9));
+  ASSERT_FALSE(rsl::is_valid_enumerator<Bar>(8));
+
+  ASSERT_TRUE(rsl::is_valid_enumerator<Baz>(Baz::foo));
+  ASSERT_TRUE(rsl::is_valid_enumerator<Baz>(Baz::bar));
+  ASSERT_TRUE(rsl::is_valid_enumerator<Baz>(Baz::foo | Baz::bar));
+  ASSERT_TRUE(rsl::is_valid_enumerator<Baz>(15));
+  ASSERT_FALSE(rsl::is_valid_enumerator<Baz>(16));
+
+  ASSERT_TRUE(rsl::is_valid_enumerator<Zoinks>(Zoinks::foo));
+  ASSERT_TRUE(rsl::is_valid_enumerator<Zoinks>(Zoinks::bar));
+  ASSERT_TRUE(rsl::is_valid_enumerator<Zoinks>(1234));
 }
