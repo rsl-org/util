138ece437f75a6fac9043cef55a9c34c5a130a6d
Tsche <che@pydong.org>
2025-12-14 02:18:18 +0000

    pull os-specific code out of primary header <rsl/print>

---
+++ include/rsl/_impl/platform/io.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#ifdef __unix__
+#  include <unistd.h>
+#elif defined(_WIN32) || defined(WIN32)
+#  define WIN32_LEAN_AND_MEAN
+#  include <windows.h>
+
+#  ifndef STDIN_FILENO
+#    define STDIN_FILENO _fileno(stdin)
+#  endif
+#  ifndef STDOUT_FILENO
+#    define STDOUT_FILENO _fileno(stdout)
+#  endif
+#  ifndef STDERR_FILENO
+#    define STDERR_FILENO _fileno(stderr)
+#  endif
+#else
+#  error "Unsupported platform"
+#endif
+
+namespace rsl::_impl_platform {
+bool isatty(int fd) {
+#if __unix__
+  return bool(::isatty(fd));
+#else
+  return bool(::_isatty(fd));
+#endif
+}
+
+}  // namespace rsl::_impl_platform
