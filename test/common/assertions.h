c99bceba832f5a11bde808d873f2709fbb117f06
Tsche <Tsche@users.noreply.github.com>
2025-12-24 01:44:06 +0000

    remove reference to broken typename utility

--- test/common/assertions.h
+++ test/common/assertions.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <gtest/gtest.h>
-#include "type_name.h"
 
 #define DEPAREN(X) ESC(ISH X)
 #define ISH(...)   ISH __VA_ARGS__
@@ -8,14 +7,12 @@
 #define ESC_(...)  VAN##__VA_ARGS__
 #define VANISH
 
-#define ASSERT_SAME(first, second)                                                                                 \
-  ASSERT_TRUE((std::is_same_v<DEPAREN(first), DEPAREN(second)>)) << "  Actual: " << nameof<DEPAREN(first)> << '\n' \
-                                                                 << "Expected: " << nameof<DEPAREN(second)>
-
-#define ASSERT_STATIC(...) ASSERT_SAME(std::bool_constant<(__VA_ARGS__)>, std::true_type)
+#define ASSERT_SAME(first, second) ASSERT_TRUE((std::is_same_v<DEPAREN(first), DEPAREN(second)>))
+#define ASSERT_STATIC(...)         ASSERT_SAME(std::bool_constant<(__VA_ARGS__)>, std::true_type)
 
 #define ASSERT_NOEXCEPT(...) ASSERT_TRUE((noexcept(__VA_ARGS__))) << "Operation must be noexcept"
-#define ASSERT_NOT_NOEXCEPT(...) ASSERT_FALSE((noexcept(__VA_ARGS__))) << "Operation must not be noexcept"
+#define ASSERT_NOT_NOEXCEPT(...) \
+  ASSERT_FALSE((noexcept(__VA_ARGS__))) << "Operation must not be noexcept"
 
 #define TEST_T(test_suite_name, test_name, ...)                \
   template <typename>                                          \
